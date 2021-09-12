15cb1815053fb079c0b418b83a7036717b9f155b
Harishankar Bhat R <rhsb1997@gmail.com>
2021-09-12 22:56:08 +0530

    Added few Lombok annotations and refactored the code.

--- Journey from Spring JDBC to JPA/JPA-Application/src/main/java/com/jpa/JpaApplication.java
+++ Journey from Spring JDBC to JPA/JPA-Application/src/main/java/com/jpa/JpaApplication.java
@@ -3,8 +3,7 @@ package com.jpa;
 import com.jpa.Dao.Person_Repository;
 import com.jpa.Entity.Person;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,12 +13,12 @@ import java.util.Date;
 
 @RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 @SpringBootApplication
+@Slf4j
 public class JpaApplication implements CommandLineRunner {
 // We are implementing CommandLineRunner class.
 // We have to implement the methods in CommandLineRunner class.
-// CommandLineRunner class is used to execute the code which is in it's methods at the time of application launch.
+// CommandLineRunner class is used to execute the code which is in its methods at the time of application launch.
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(JpaApplication.class);
 	private static final String SEPARATOR = "-----------------------------------------------------------------------------";
 
 	private final Person_Repository person_repository;
@@ -32,19 +31,19 @@ public class JpaApplication implements CommandLineRunner {
 	public void run(String... args) {
 
 		printSeparator();
-		LOGGER.info("\n User Id 10001 -> {}", person_repository.findById(10001));
+		log.info("\n User Id 10001 -> {}", person_repository.findById(10001));
 		printSeparator();
 
-		LOGGER.info("\n Inserting 10006 -> {}", person_repository.insert(new Person("Smith", "Chennai", new Date())));
+		log.info("\n Inserting 10006 -> {}", person_repository.insert(new Person("Smith", "Chennai", new Date())));
 		printSeparator();
 
-		LOGGER.info("\n Updating 10003 -> {}", person_repository.update(new Person(10003, "Jill", "Singapore", new Date())));
+		log.info("\n Updating 10003 -> {}", person_repository.update(new Person(10003, "Jill", "Singapore", new Date())));
 		person_repository.delete(10002);
 		// We can't put this in logger.info because this method doesn't return anything.
 		// So we can't print anything.
 		printSeparator();
 
-		LOGGER.info("All Users -> {}", person_repository.findAll());
+		log.info("All Users -> {}", person_repository.findAll());
 		printSeparator();
 	}
 
